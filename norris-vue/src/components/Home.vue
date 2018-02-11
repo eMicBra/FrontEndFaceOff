cedad33417b1a0716ddbca0558b9758e614b0a19
Chris Fuess <cfuess@LIVE.COM>
2018-02-11 14:01:44 -0800

    Made background black, added js-cookie to packages, added Video component to home page controled by toggle and cookie state.
    Modified CSS, made black background go all the way to the bottom.

--- norris-vue/src/components/Home.vue
+++ norris-vue/src/components/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <h2>{{msg}}</h2>
-    <img src="../assets/Chuck-Norris-HD.jpg" style="width: 400px;">
+    <coolVideo></coolVideo>
+    <h2 class="title">{{msg}}</h2>
+    <img class="image" src="../assets/Chuck-Norris-HD.jpg" style="width: 400px;">
     <rjoke></rjoke>
     <hr />
     <Cat>
@@ -12,34 +13,42 @@
 <script>
 import Categories from './Categories'
 import RandomJoke from './Joke'
+import Video from './Video'
+
 export default {
   name: 'Home',
   components: {
     Cat: Categories,
-    rjoke: RandomJoke
+    rjoke: RandomJoke,
+    coolVideo: Video
   },
+
   data () {
     return {
       msg: 'Enlightenment! Compliments of Chuck Norris.'
     }
   }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  .title {
+    /*padding-bottom: 10px;*/
+    margin: 0px 0 10px 0;
+  }
+
+  .image {
+    width: 400px;
+    margin: 10px 0 10px 0;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
